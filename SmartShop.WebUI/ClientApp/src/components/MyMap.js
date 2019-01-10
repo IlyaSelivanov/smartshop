f8c4bb196ff1d898542a01e43597be52ff38f026
Ilya Selivanov <ilyaqs@quartsoft.com>
2019-01-10 15:29:05 +0200

    clientapp: mymap current location

--- SmartShop.WebUI/ClientApp/src/components/MyMap.js
+++ SmartShop.WebUI/ClientApp/src/components/MyMap.js
@@ -1,29 +1,56 @@
 ﻿import React, { Component } from 'react';
 import { GoogleMap, Marker, withGoogleMap } from 'react-google-maps';
+import { Container, Row, Col } from 'reactstrap';
+
+const geolocation = (
+    navigator.geolocation || {
+        getCurrentPosition: (success, failure) => {
+            failure("Your browser doesn't support geolocation.");
+        },
+    }
+);
 
 export class MyMap extends Component {
     static displayName = MyMap.name;
 
     constructor(props) {
         super(props);
-        this.state = { loading: true };
+        this.state = {
+            loading: true,
+            center: null
+        };
+    }
+
+    componentDidMount() {
+        geolocation.getCurrentPosition((position) => {
+            this.setState({
+                center: {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                }
+            })
+        });
     }
 
     render() {
         let GoogleMapExample = withGoogleMap(props => (
             <GoogleMap
-                defaultCenter={{ lat: 40.756795, lng: -73.954298 }}
-                defaultZoom={13}
+                defaultCenter={this.state.center}
+                defaultZoom={15}
             >
             </GoogleMap>
         ));
         return (
-            <div>
-                <GoogleMapExample
-                    containerElement={<div style={{ height: `500px`, width: '500px' }} />}
-                    mapElement={<div style={{ height: `100%` }} />}
-                />
-            </div>
+            <Container>
+                <Row>
+                    <Col>
+                        <GoogleMapExample
+                            containerElement={<div style={{ height: `500px`, width: '500px' }} />}
+                            mapElement={<div style={{ height: `100%` }} />}
+                        />
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
